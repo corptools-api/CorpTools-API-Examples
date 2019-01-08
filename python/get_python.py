02b068f5565f471f1de81d4f251f82a8c2bf3d91
Rachel Lanman <rachell@corporatetools.com>
2019-01-08 15:29:35 -0800

    get and post python examples

---
+++ python/get_python.py
@@ -0,0 +1,31 @@
+import logging
+import hashlib
+import json
+import jwt
+import requests
+
+ACCESS_KEY = 'MXU58QLVEN2B7Z3G4O1R'
+SECRET_KEY = 'j2zffy+cDIeZ6qgDZ3uJOA=='
+
+logger = logging.getLogger(__name__)
+
+path = "/compliance-events"
+body = json.dumps({ 'start_date': 'Aug 1, 2018', 'end_date': 'Dec 31, 2018', 'limit': 40 })
+
+headers = {"access_key": ACCESS_KEY}
+payload = {
+    "path": path,
+    "content": hashlib.sha256(body.encode('ascii')).hexdigest()
+}
+
+token = jwt.encode(payload, SECRET_KEY, algorithm='HS256', headers=headers)
+
+try:
+    url = "https://api.corporatetools.com/compliance-events"
+    headers = {"Authorization": 'Bearer %s'%(token), "Content-Type": "json"}
+    r = requests.get(url, headers=headers, data=body)
+
+    print(r.json())
+except Exception as e:
+    logger.error(msg="{e}".format(e=e))
+    raise e
