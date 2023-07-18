957e79de9b7f5e1e9999d7ad47ad77883b6acfe8
Madison de Groot <madisondegroot@corporatetools.com>
2023-07-18 15:39:57 -0700

    javascript examples and script shorthand

---
+++ javascript/examples/get_filing_products.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const WEBSITE_URL = process.env.WEBSITE_URL;
+const JURISDICTION = process.env.JURISDICTION;
+
+params = {
+  url: WEBSITE_URL,
+  jurisdiction: JURISDICTION
+}
+
+token = require('../request.js').request.token({ path: '/filing-products' });
+
+require('../request.js').request.get({ path: '/filing-products', token: token, queryParams: params });
