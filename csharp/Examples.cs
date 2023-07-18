fb44ff2c42e3428049eb69cf72a422149bfe861b
Matt Perry <mattperry@corporatetools.com>
2023-07-18 16:58:30 -0700

    FILE-2192: reusable Request class for C#

--- csharp/Examples.cs
+++ csharp/Examples.cs
@@ -2,44 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net;
-using System.Security.Cryptography;
-using System.Text;
-using Microsoft.IdentityModel.Tokens;
-using RestSharp;
 using dotenv;
 
+
 namespace Examples
-{
+{   
     class Program
     {
-        private static string AccessKey;
-        private static string SecretKey;
-        private static string ApiUrl;
-        private static string CallbackUrl;
-        
         // Here we have examples of how to add a callback to the API, getting a list of all callbacks added, and an example
         // of what it would look like to listen for that callback to trigger and then how to decode the corresponding data
         static void Main(string[] args)
         {
-            setupEnvironment();
-            // AddCallback();
+            //AddCallback();
             GetCallbacks();
-            ListenForCallback();
+            //ListenForCallback();
         }
 
-        private static void setupEnvironment()
-        {
-            dotenv.net.DotEnv.Load();
-            AccessKey = Environment.GetEnvironmentVariable("ACCESS_KEY");
-            SecretKey = Environment.GetEnvironmentVariable("SECRET_KEY");
-            ApiUrl = Environment.GetEnvironmentVariable("API_URL");
-            CallbackUrl = Environment.GetEnvironmentVariable("CALLBACK_URL");
-        }
-
+       
         private static void ListenForCallback()
         {
+            dotenv.net.DotEnv.Load();
+            string callbackUrl = Environment.GetEnvironmentVariable("CALLBACK_URL");
             HttpListener listener = new HttpListener();
-            listener.Prefixes.Add(CallbackUrl);
+            listener.Prefixes.Add(callbackUrl);
             listener.Start();
             
             HttpListenerContext context   = listener.GetContext();
@@ -60,68 +45,21 @@ namespace Examples
 
         private static void AddCallback()
         {
-            var path    = "callbacks";
-            var client  = new RestClient(ApiUrl);
-            var request = new RestRequest(path, RestSharp.Method.Post);
-            request.RequestFormat = DataFormat.Json;
-            
+            dotenv.net.DotEnv.Load();
+            string callbackUrl = Environment.GetEnvironmentVariable("CALLBACK_URL");
             var body = Newtonsoft.Json.JsonConvert.SerializeObject(new
             {
-                url = CallbackUrl
+                url = callbackUrl
             });
-            
-            GenerateJwtToken(ref request, path, body);
 
-            var response = client.Post(request);
-                
-            Console.WriteLine(response.Content);
-            
+            Request request = new Request();
+            request.PostRequest("callbacks", body);
         }
 
         private static void GetCallbacks()
         {
-            var path    = "callbacks";
-            var client  = new RestClient(ApiUrl);
-            var request = new RestRequest(path, RestSharp.Method.Get);
-            request.RequestFormat = DataFormat.Json;
-            
-            GenerateJwtToken(ref request, path);
-
-            var response = client.Get(request);
-                
-            Console.WriteLine(response.Content);
-        }
-
-        private static void GenerateJwtToken(ref RestRequest request, string path, string body="")
-        {
-            var key         = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var header      = new JwtHeader(credentials)
-            {
-                {"access_key", AccessKey}
-            };
-            var payload = new JwtPayload
-            {
-                {"path", "/" + path},
-                {"content", Sha256HexDigest(body) }
-            };
-            
-            var securityToken = new JwtSecurityToken(header, payload);
-            var handler       = new JwtSecurityTokenHandler();
-            var token   = handler.WriteToken(securityToken);
-            
-            request.AddHeader("Authorization", "Bearer " + token);
-            request.AddHeader("Content-Type", "application/json");
-            if (body != "")
-            {
-                request.AddJsonBody(body);
-            }
-        }
-
-        private static string Sha256HexDigest(string body)
-        {
-            return BitConverter.ToString(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(body)))
-                .Replace("-", String.Empty).ToLower();
+            Request request = new Request();
+            request.GetRequest("callbacks", "");
         }
     }
 }
