d8d768f51fce6f018202d5ec3f0da32fa8ef2427
Corptools API <63736943+corporatetools@users.noreply.github.com>
2023-07-19 10:55:05 -0700

    Feature/file 2192 product offering csharp (#13)
    * FILE-2192: BaseRequest pattern for reuse, filing products offerings, get callbacks, and post callbacks examples
    ---------
    
    Co-authored-by: Matt Perry <mattperry@corporatetools.com>

---
+++ csharp/examples/BaseRequest.cs
@@ -0,0 +1,82 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using RestSharp;
+
+namespace Examples
+{
+    /*
+     * Base Request class with examples of how to generate the JWT token and make generic HTTP requests 
+     */
+    public abstract class BaseRequest
+    {
+        protected static string _accessKey;
+        protected static string _secretKey;
+        protected static string _apiUrl;
+
+        protected BaseRequest()
+        {
+            dotenv.net.DotEnv.Load();
+            _accessKey = Environment.GetEnvironmentVariable("ACCESS_KEY");
+            _secretKey = Environment.GetEnvironmentVariable("SECRET_KEY");
+            _apiUrl = Environment.GetEnvironmentVariable("API_URL");
+            Console.WriteLine($"BaseRequest: _apiUrl={_apiUrl} _accessKey={_accessKey} _secretKey={_secretKey}");
+        }
+
+        public abstract void SendRequest();
+
+        protected void GetRequest(string path)
+        {
+            var client = new RestClient(_apiUrl);
+            var request = new RestRequest(path, Method.Get);
+            request.RequestFormat = DataFormat.Json;
+            GenerateJwtToken(ref request, path);
+            var response = client.Get(request);
+            Console.WriteLine(response.Content);
+        }
+
+        protected void PostRequest(string path, string body)
+        {
+            var client = new RestClient(_apiUrl);
+            var request = new RestRequest(path, Method.Post);
+            request.RequestFormat = DataFormat.Json;
+            GenerateJwtToken(ref request, path, body);
+            var response = client.Post(request);
+            Console.WriteLine(response.Content);
+        }
+
+        protected void GenerateJwtToken(ref RestRequest request, string path, string body = "")
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var header = new JwtHeader(credentials)
+            {
+                { "access_key", _accessKey }
+            };
+            var payload = new JwtPayload
+            {
+                {"path", "/" + path},
+                {"content", Sha256HexDigest(body) }
+            };
+
+            var securityToken = new JwtSecurityToken(header, payload);
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.WriteToken(securityToken);
+
+            request.AddHeader("Authorization", "Bearer " + token);
+            request.AddHeader("Content-Type", "application/json");
+            if (body != "")
+            {
+                request.AddJsonBody(body);
+            }
+        }
+
+        internal static string Sha256HexDigest(string body)
+        {
+            return BitConverter.ToString(SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(body)))
+                .Replace("-", String.Empty).ToLower();
+        }
+    }
+}
---
+++ csharp/examples/GetFilingProductsOfferings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Examples.examples
+{
+    // GET /filing-products/offerings
+    class GetFilingProductsOfferings : BaseRequest
+    {
+		private string _companyId;
+        private string _jurisdiction;
+
+        public GetFilingProductsOfferings() : base()
+		{
+            dotenv.net.DotEnv.Load();
+            _companyId      = Environment.GetEnvironmentVariable("COMPANY_ID");
+            _jurisdiction   = Environment.GetEnvironmentVariable("JURISDICTION");
+            Console.WriteLine($"GetFilingProductsOfferings: _companyId={_companyId} _jurisdiction={_jurisdiction}");
+        }
+
+        public override void SendRequest()
+        {
+            string path = "filing-products/offerings";
+            string queryParams = $"?company_id={_companyId}&jurisdiction={_jurisdiction}";
+            GetRequest(path + queryParams);
+        }
+	}
+}
