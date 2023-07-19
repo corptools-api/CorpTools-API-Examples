f42f17de74ffde1ca2e1ed09c2bae5b834fc5f70
Madison de Groot <madisondegroot@corporatetools.com>
2023-07-19 13:26:52 -0700

    pulled out base request for examples in python and added filing-products get request

---
+++ python/examples/get_filing_products.py
@@ -0,0 +1,22 @@
+import pprint
+from request import BaseRequest
+from dotenv import dotenv_values
+
+config = dotenv_values()
+
+class GetFilingProductsRequest(BaseRequest):
+    def __init__(self):
+        super().__init__()
+    
+    def get_filing_products(self, url, jurisdiction):
+        path = "/filing-products"
+        body = { 'url': url, 'jurisdiction': jurisdiction }
+        return self.make_request("GET", path, body)
+
+url = config['WEBSITE_URL']
+jurisdiction = config['JURISDICTION']
+
+filing_products_request = GetFilingProductsRequest()
+filing_products = filing_products_request.get_filing_products(url, jurisdiction)
+
+pprint.pprint(filing_products)
