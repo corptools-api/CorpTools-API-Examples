3a982243ff5f2d87dd1144a26239471dbbb58350
Matt Perry <mattperry@corporatetools.com>
2023-07-19 12:14:33 -0700

    FILE-2192: GET /filing-products/offerings example

--- php/examples/get_filing_products_offerings.php
+++ php/examples/get_filing_products_offerings.php
@@ -1,3 +1,10 @@
 <?php
+require_once 'base_request_curl.php';
 
-echo "TODO: GET /filing-products/offerings";
+$debug = true;
+$jurisdiction = $_ENV['JURISDICTION'];
+$company_id   = $_ENV['COMPANY_ID'];
+$request_data = '';
+$request_params = ['jurisdiction' => $jurisdiction, 'company_id' => $company_id];
+
+send_request('GET', '/filing-products/offerings', $request_params, $request_data);
