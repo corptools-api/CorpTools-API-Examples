91664c71faa7730a9215e8af3e32eecba5bc6bff
Matt Perry <mattperry@corporatetools.com>
2023-07-19 15:05:25 -0700

    FILE-2213: javascript-axios POST /shopping-cart

---
+++ javascript-axios/examples/post_shopping_cart.js
@@ -0,0 +1,23 @@
+const path = require('path');
+process.chdir(path.resolve(__dirname, '../../'));
+require('dotenv').config();
+
+// Example of POST /shopping-cart
+
+const COMPANY_ID        = process.env.COMPANY_ID;
+const FILING_PRODUCT_ID = process.env.FILING_PRODUCT_ID;
+const FILING_METHOD_ID  = process.env.FILING_METHOD_ID;
+const QUANTITY = 1;
+// const FORM_DATA = {}; // optional, expects JSON mapping to fields of filing method schema
+
+let body = { 
+  company_id: COMPANY_ID,
+  product_id: FILING_PRODUCT_ID,
+  product_option_id: FILING_METHOD_ID,
+  quantity: QUANTITY,
+  // form_data: FORM_DATA
+}; 
+
+token = require('../request.js').request.token({ path: '/shopping-cart', body: body });
+
+require('../request.js').request.post({ path: '/shopping-cart', token: token, body: body });
