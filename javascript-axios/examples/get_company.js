e5afb033419110ad958ce1c432ac1cda7409a311
Corptools API <63736943+corporatetools@users.noreply.github.com>
2023-07-20 13:00:25 -0700

    built out examples for get-companies and get-company endpoints (#22)
    * built out examples for get-companies and get-company endpoints
    
    * syntax fix for companies path
    
    ---------
    
    Co-authored-by: Madison de Groot <madisondegroot@corporatetools.com>

---
+++ javascript-axios/examples/get_company.js
@@ -0,0 +1,9 @@
+const path = require('path');
+process.chdir(path.resolve(__dirname, '../../'));
+require('dotenv').config();
+
+const COMPANY_ID	= process.env.COMPANY_ID;
+
+token = require('../request.js').request.token({ path: `/companies/${COMPANY_ID}` });
+
+require('../request.js').request.get({ path: `/companies/${COMPANY_ID}`, token: token });
