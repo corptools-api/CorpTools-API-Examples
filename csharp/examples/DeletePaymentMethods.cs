1887a568b74f19faf41b8af556c3127e277f6537
Matt Perry <mattperry@corporatetools.com>
2023-07-20 15:20:11 -0700

    FILE-2200: DELETE /payment-methods in C#

---
+++ csharp/examples/DeletePaymentMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Examples.examples
+{
+    // Example of DELETE /payment-methods/:id
+	public class DeletePaymentMethods : BaseRequest
+    {
+        private string _paymentMethodId;
+
+        public DeletePaymentMethods()
+		{
+            dotenv.net.DotEnv.Load();
+            _paymentMethodId = Environment.GetEnvironmentVariable("PAYMENT_METHOD_ID");
+            Console.WriteLine($"GetFilingProducts: _paymentMethodId={_paymentMethodId}");
+        }
+
+        public override void SendRequest()
+        {
+            DeleteRequest($"payment-methods/{_paymentMethodId}");
+        }
+    }
+}
+
