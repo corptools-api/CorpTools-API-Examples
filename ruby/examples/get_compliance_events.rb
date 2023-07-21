2cbd7736da42bf188af5dc6b565efed4a26c8f07
Madison de Groot <madisondegroot@corporatetools.com>
2023-07-21 14:23:40 -0700

    [FILE-2182] build out remaining get-compliance-events examples

---
+++ ruby/examples/get_compliance_events.rb
@@ -0,0 +1,18 @@
+require_relative '../request.rb'
+
+# Lists compliance events for specified company
+# Either a company or company_id may be provided, but not both
+# Either an array of jurisdictions or jurisdiction_ids may be provided, but not both
+# Start_date and end_date are required
+
+COMPANY_ID        = ENV['COMPANY_ID']
+START_DATE        = ENV['START_DATE']
+END_DATE          = ENV['END_DATE']
+
+params = {
+  company_id: COMPANY_ID,
+  start_date: START_DATE,
+  end_date: END_DATE
+}
+
+BaseRequestRoute.request(:get, '/compliance-events', query_params: params)
