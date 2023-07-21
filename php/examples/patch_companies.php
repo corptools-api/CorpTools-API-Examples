d67a428f1f3aa1d60e19d51d1b5ed5d5c97a2286
Corptools API <63736943+corporatetools@users.noreply.github.com>
2023-07-21 13:08:29 -0700

    Feature/file 2180 patch companies (#31)
    * [FILE-2180] built out patch requests for ruby php and python
    
    * [FILE-2180] completed patch-companies examples for all projects
    
    * [FILE-2180] removed duplicated code
    
    ---------
    
    Co-authored-by: Madison de Groot <madisondegroot@corporatetools.com>

---
+++ php/examples/patch_companies.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'base_request.php';
+
+// Example of PATCH /companies
+
+// this example request will update the home_state for the specified company
+$debug = true;
+$company = $_ENV['COMPANY_NAME'];
+$entity_type = 'Corporation';
+
+$request_data = array(
+    'company' => $company,
+    'entity_type' => $entity_type,
+);
+
+$companies_data = array(
+  'companies' => array($request_data)
+);
+
+$request_params = '';
+
+$json_body = json_encode($companies_data);
+
+send_request('PATCH', '/companies', $request_params, $json_body);
