53f7a7a6eb09ea494de92b480874869d1c27ae25
Madison de Groot <madisondegroot@corporatetools.com>
2023-07-26 15:03:13 -0700

    [FILE-2195] completed pay invoices examples

---
+++ csharp/examples/PostInvoicesPay.cs
@@ -0,0 +1,28 @@
+using System;
+namespace Examples.examples
+{
+    // POST /invoices/pay
+    public class PostInvoicesPay : BaseRequest
+    {
+        private string _paymentToken;
+        private string[] _invoiceIds;
+
+        public PostInvoicesPay()
+		{
+            _paymentToken = Environment.GetEnvironmentVariable("PAYMENT_METHOD_ID");
+            _invoiceIds = Environment.GetEnvironmentVariable("INVOICE_ID")?.Split(',');
+            Console.WriteLine($"PostInvoicesPay: _paymentToken={_paymentToken} _invoiceIds={_invoiceIds}");
+        }
+
+        public override void SendRequest()
+        {
+            var body = Newtonsoft.Json.JsonConvert.SerializeObject(new
+            {
+                payment_token = _paymentToken,
+                invoice_ids = _invoiceIds,
+            });
+
+            PostRequest("invoices/pay", body);
+        }
+    }
+}
