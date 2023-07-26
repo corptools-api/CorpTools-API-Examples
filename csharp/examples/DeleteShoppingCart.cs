5054911db36b65339b17166f1a492cc2c2dde2f6
Matt Perry <mattperry@corporatetools.com>
2023-07-26 08:53:44 -0700

    FILE-2177: DELETE /callbacks/:id in C#

--- csharp/examples/DeleteShoppingCart.cs
+++ csharp/examples/DeleteShoppingCart.cs
@@ -12,7 +12,7 @@ namespace Examples.examples
             dotenv.net.DotEnv.Load();
             _companyId = Environment.GetEnvironmentVariable("COMPANY_ID");
             _shoppingCartItemId = Environment.GetEnvironmentVariable("SHOPPING_CART_ITEM_ID");
-            Console.WriteLine($"GetFilingProductsOfferings: _companyId={_companyId} _shoppingCartItemId={_shoppingCartItemId}");
+            Console.WriteLine($"DeleteShoppingCart: _companyId={_companyId} _shoppingCartItemId={_shoppingCartItemId}");
         }
 
 		public override void SendRequest()
