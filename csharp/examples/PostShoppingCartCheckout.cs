04a247dd1598f5fd3611afb1dcdc5aa32e6e7840
Matt Perry <mattperry@corporatetools.com>
2023-07-26 13:45:04 -0700

    FILE-2215: POST /shopping-cart/checkout in C#

---
+++ csharp/examples/PostShoppingCartCheckout.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Examples.examples
+{
+    // POST /shopping-cart/checkout
+    public class PostShoppingCartCheckout : BaseRequest
+	{
+        private string _companyId;
+        private string _paymentMethodId;
+        private string _itemId;
+
+        public PostShoppingCartCheckout()
+		{
+            _companyId = Environment.GetEnvironmentVariable("COMPANY_ID");
+            _itemId = Environment.GetEnvironmentVariable("COMPANY_ID");
+            _paymentMethodId = Environment.GetEnvironmentVariable("COMPANY_ID");
+            Console.WriteLine($"PostShoppingCartCheckout: _companyId={_companyId} _itemId={_itemId} _paymentMethodId={_paymentMethodId}");
+        }
+
+        public override void SendRequest()
+        {
+            string[] item_ids = { _itemId };
+            string[] company_ids = { _companyId };
+
+            var body = Newtonsoft.Json.JsonConvert.SerializeObject(new
+            {
+                company_ids = company_ids,
+                item_ids = item_ids,
+                payment_token = _paymentMethodId
+            });
+
+            PostRequest("shopping-cart/checkout", body);
+        }
+    }
+}
+
