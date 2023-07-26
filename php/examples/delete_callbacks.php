1bfbdbaa96ffe61cc9e008b9abf93ef86a2027e8
Matt Perry <mattperry@corporatetools.com>
2023-07-26 09:10:37 -0700

    FILE-2177: DELETE /callbacks/:id in PHP

---
+++ php/examples/delete_callbacks.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'base_request.php';
+
+// Example of DELETE /callbacks/:id
+
+$debug = true;
+$callback_id = $_ENV['CALLBACK_ID'];
+$path = '/callbacks/' . $callback_id;
+
+send_request('DELETE', $path, '', '');
--- php/examples/delete_payment_methods.php
+++ php/examples/delete_payment_methods.php
@@ -5,10 +5,6 @@ require_once 'base_request.php';
 
 $debug = true;
 $payment_method_id = $_ENV['PAYMENT_METHOD_ID'];
-$request_params = [];
-$request_data = '';
-$json_body = json_encode($request_data);
-
 $path = '/payment-methods/' . $payment_method_id;
 
 send_request('DELETE', $path, '', '');
