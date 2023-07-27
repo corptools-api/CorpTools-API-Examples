fd5fad191552e83c1b39a17668e1ebcda61d64e5
Matt Perry <mattperry@corporatetools.com>
2023-07-27 15:18:08 -0700

    FILE-2207: GET /services limit and offset params

--- php/examples/get_services.php
+++ php/examples/get_services.php
@@ -6,10 +6,14 @@ require_once 'base_request.php';
 $debug = true;
 $company_id   = $_ENV['COMPANY_ID'];
 $company_name   = $_ENV['COMPANY_NAME'];
+$limit = 3;
+$offset = 0;
 $request_data = '';
 $request_params = [
 	'company_id' => $company_id,
-	// 'company' => $company
+	// 'company' => $company,
+	// 'limit' => $limit,
+	// 'offset' => $offset
 ];
 
 send_request('GET', '/services', $request_params, $request_data);
--- python/examples/get_services.rb
+++ python/examples/get_services.rb
@@ -15,17 +15,21 @@ class GetServicesRequest(BaseRequest):
     def __init__(self):
         super().__init__()
     
-    def get_services(self, company_id, company_name):
+    def get_services(self, company_id, company_name, limit, offset):
         params = {
             'company_id': company_id,
-            # 'company_name': company_name
+            # 'company_name': company_name,
+            # 'limit': limit,
+            # 'offset': offset
         }
         return self.make_request('GET', '/services', params=params)
 
 company_id  = config['COMPANY_ID']
 company_name  = config['COMPANY_NAME']
+limit = 3
+offset = 0
 
 request = GetServicesRequest()
-response = request.get_services(company_id, company_name)
+response = request.get_services(company_id, company_name, limit, offset)
 
 pprint.pprint(response)
