08581c78d4de87380c36b3a6167fdd399c946fc2
Matt Perry <mattperry@corporatetools.com>
2023-07-27 13:35:19 -0700

    FILE-2167: GET /account/dashhpanel in Python

---
+++ python/examples/get_account_dashpanel.py
@@ -0,0 +1,22 @@
+import pprint
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from request import BaseRequest
+
+# Example of GET /account
+
+class GetAccountDashPanelRequest(BaseRequest):
+    def __init__(self):
+        super().__init__()
+    
+    def get_account_dashpanel(self):
+        path = "/account/dashpanel"
+        return self.make_request("GET", path)
+
+request = GetAccountDashPanelRequest()
+account = request.get_account_dashpanel()
+
+pprint.pprint(account)
