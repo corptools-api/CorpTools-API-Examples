3c683f1acb7f300b261e2dd82cd4305ffedd97a2
Madison de Groot <madisondegroot@corporatetools.com>
2023-07-27 14:48:08 -0700

    [FILE-2184] built out get-document download for all projects and allowing for non-JSON responses

---
+++ javascript/examples/get_document_download.js
@@ -0,0 +1,12 @@
+const baseRequest = require('../base_request.js').baseRequest
+const path = require('path');
+process.chdir(path.resolve(__dirname, '../../'));
+require('dotenv').config();
+
+// Example of GET /documents/:document_id/download
+
+const DOCUMENT_ID = process.env.DOCUMENT_ID;
+
+token = baseRequest.request.token({ path: `/documents/${DOCUMENT_ID}/download` });
+
+baseRequest.request.get({ path: `/documents/${DOCUMENT_ID}/download`, token: token });
