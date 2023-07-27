4c1585b3aa5231d1d7e6a21d04c81cdafa8732e3
Matt Perry <mattperry@corporatetools.com>
2023-07-27 14:14:52 -0700

    FILE-2216: GET /signed-forms in C#

---
+++ csharp/examples/GetSignedForms.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Examples.examples
+{
+    // Example of GET /signed-forms
+    public class GetSignedForms : BaseRequest
+    {
+        private string _filingMethodId;
+        private string _websiteId;
+
+        public GetSignedForms()
+        {
+            dotenv.net.DotEnv.Load();
+            _filingMethodId = Environment.GetEnvironmentVariable("FILING_METHOD_ID");
+            _websiteId = Environment.GetEnvironmentVariable("WEBSITE_ID");
+
+            Console.WriteLine($"GetSignedForms: _filingMethodIde={_filingMethodId} _websiteId={_websiteId}");
+        }
+
+
+        public override void SendRequest()
+        {
+            GetRequest($"signed-forms?filing_method_id={_filingMethodId}&website_id={_websiteId}");
+        }
+    }
+    
+}
+
