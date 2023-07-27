7e174601a9d46951f48409ccefe16ed6248e9b22
Corptools API <63736943+corporatetools@users.noreply.github.com>
2023-07-27 13:35:49 -0700

    Feature/file 2164 get accounts (#45)
    * FILE-2164: GET /account in JavaScript
    
    * FILE-2164: GET /account in Ruby, filename fix
    
    * FILE-2164: GET /account in JavaScript
    
    * FILE-2164: GET /account in Python
    
    * fix comment and remove unused imports
    
    * FILE-2164: GET /account in PHP
    
    * FILE-2164: GET /account in C#
    
    ---------
    
    Co-authored-by: Matt Perry <mattperry@corporatetools.com>

---
+++ csharp/examples/GetAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Examples.examples
+{
+    // Example of GET /account
+	public class GetAccount : BaseRequest
+	{
+		public GetAccount()
+		{
+		}
+
+        public override void SendRequest()
+        {
+            GetRequest("account");
+        }
+    }
+}
