2b2290ca4af2d26e18f7f97ea4e8ba14bcc4a1a5
Corptools API <63736943+corporatetools@users.noreply.github.com>
2023-07-27 13:47:11 -0700

    Feature/file 2167 account dashboard (#47)
    * FILE-2164: GET /account in JavaScript
    
    * FILE-2164: GET /account in Ruby, filename fix
    
    * FILE-2164: GET /account in JavaScript
    
    * FILE-2164: GET /account in Python
    
    * fix comment and remove unused imports
    
    * FILE-2164: GET /account in PHP
    
    * FILE-2164: GET /account in C#
    
    * FILE-2167: GET /account/dashhpanel in JavaScript
    
    * FILE-2167: GET /account/dashhpanel in Python
    
    * FILE-2167: GET /account/dashhpanel in PHP
    
    * FILE-2167: GET /account/dashhpanel in C#
    
    ---------
    
    Co-authored-by: Matt Perry <mattperry@corporatetools.com>

---
+++ csharp/examples/GetAccountDashPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Examples.examples
+{
+    // Example of GET /account/dashpanel
+    public class GetAccountDashPanel : BaseRequest
+    {
+        public GetAccountDashPanel()
+        {
+        }
+
+        public override void SendRequest()
+        {
+            GetRequest("account/dashpanel");
+        }
+    }
+}
