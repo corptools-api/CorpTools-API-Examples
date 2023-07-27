aed60ed9e29509e0ae17756196bee9a18d4b1dd5
Madison de Groot <madisondegroot@corporatetools.com>
2023-07-27 13:00:17 -0700

    [FILE-2185] built out get-document by id examples for all projects

---
+++ python/examples/get_document.py
@@ -0,0 +1,27 @@
+import pprint
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from request import BaseRequest
+from dotenv import dotenv_values
+
+config = dotenv_values()
+
+# Example of GET /documents/:document_id
+
+class GetDocumentRequest(BaseRequest):
+    def __init__(self):
+        super().__init__()
+
+    def get_document(self):
+        path = f"/documents/{document_id}"
+        return self.make_request("GET", path)
+
+document_id = config['DOCUMENT_ID']
+
+get_document_request = GetDocumentRequest()
+document = get_document_request.get_document()
+
+pprint.pprint(document)
