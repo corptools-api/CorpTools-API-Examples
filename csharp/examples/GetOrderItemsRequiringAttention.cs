d82d3a2a1c21a0187ec95c7c7002f5767cb2a236
Corptools API <63736943+corporatetools@users.noreply.github.com>
2023-07-27 09:08:15 -0700

    Feature/file 2196 get ora (#42)
    * FILE-2196: GET /order-items/requiring-attention in Ruby
    
    * FILE-2196: GET /order-items/requiring-attention in Python
    
    * FILE-2196: GET /order-items/requiring-attention in JavaScript
    
    * FILE-2196: GET /order-items/requiring-attention in PHP
    
    * FILE-2196: GET /order-items/requiring-attention in C#
    
    ---------
    
    Co-authored-by: Matt Perry <mattperry@corporatetools.com>

---
+++ csharp/examples/GetOrderItemsRequiringAttention.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Examples.examples
+{
+    // Example of GET /order-items/requiring-attention
+    public class GetOrderItemsRequiringAttention : BaseRequest
+	{
+        private string _companyId;
+
+        public GetOrderItemsRequiringAttention()
+		{
+            dotenv.net.DotEnv.Load();
+            _companyId = Environment.GetEnvironmentVariable("COMPANY_ID");
+            Console.WriteLine($"GetOrderItemsRequiringAttention: _companyId={_companyId}");
+        }
+
+        public override void SendRequest()
+        {
+            GetRequest($"order-items/requiring-attention?company_ids={_companyId}");
+        }
+    }
+}
+
