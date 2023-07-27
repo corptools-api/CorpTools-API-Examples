c902c06466879d5121ea9ad9f2c045f01ed44f1e
Corptools API <63736943+corporatetools@users.noreply.github.com>
2023-07-27 13:19:45 -0700

    Feature/file 2197 post ora (#44)
    * FILE-2196: GET /order-items/requiring-attention in Ruby
    
    * FILE-2196: GET /order-items/requiring-attention in Python
    
    * FILE-2196: GET /order-items/requiring-attention in JavaScript
    
    * FILE-2196: GET /order-items/requiring-attention in PHP
    
    * FILE-2196: GET /order-items/requiring-attention in C#
    
    * FILE-2196: POST /order-items/requiring-attention in Ruby
    
    * FILE-2196: POST /order-items/requiring-attention in JavaScript, moved data into root dir
    
    * FILE-2196: POST /order-items/requiring-attention in Python
    
    * remove copy/past comments
    
    * FILE-2196: POST /order-items/requiring-attention in C#
    
    * FILE-2196: POST /order-items/requiring-attention in PHP
    
    * FILE-2196: fix file to read from
    
    ---------
    
    Co-authored-by: Matt Perry <mattperry@corporatetools.com>

---
+++ ruby/examples/post_order_items_requiring_attention.rb
@@ -0,0 +1,17 @@
+require_relative '../request.rb'
+
+# Example of POST /order-items/requiring-attention
+
+COMPANY_ID    = ENV['COMPANY_ID']
+ORDER_ITEM_ID = ENV['ORDER_ITEM_ID']
+# replace with JSON file for the specific filing
+current_directory="#{File.dirname(File.expand_path(__FILE__))}"
+FORM_DATA = JSON.parse(File.read("#{current_directory}/../../data/form_data.json"))
+
+params = {
+  'company_id': COMPANY_ID,
+  'form_data': FORM_DATA,
+  'order_item_id': ORDER_ITEM_ID
+}.to_json
+
+BaseRequestRoute.request(:post, '/order-items/requiring-attention', body: params)
