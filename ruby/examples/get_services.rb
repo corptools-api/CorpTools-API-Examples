0cfb4dd035ef8174858c55f347bb7e0738f9d907
Matt Perry <mattperry@corporatetools.com>
2023-07-27 14:44:44 -0700

    FILE-2207: GET /services in Ruby

---
+++ ruby/examples/get_services.rb
@@ -0,0 +1,17 @@
+require_relative '../request.rb'
+
+# Example of GET /services
+
+COMPANY_ID = ENV['COMPANY_ID']
+COMPANY_NAME = ENV['COMPANY_NAME']
+LIMIT = 3
+OFFSET = 0
+
+params = {
+	# company_id: COMPANY_ID,
+	company: COMPANY_NAME,
+	# limit: LIMIT,
+	# offset: OFFSET
+}
+
+BaseRequestRoute.request(:get, '/services', query_params: params)
