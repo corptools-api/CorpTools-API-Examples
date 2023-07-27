89ab344ddb3539937f0534a1869c747586bfdf10
Matt Perry <mattperry@corporatetools.com>
2023-07-27 13:59:43 -0700

    FILE-2216: GET /signed-forms in JavaScript

---
+++ javascript/examples/get_signed_forms.js
@@ -0,0 +1,18 @@
+const baseRequest = require('../base_request.js').baseRequest
+const path = require('path');
+process.chdir(path.resolve(__dirname, '../../'));
+require('dotenv').config();
+
+// Example GET /signed-forms
+
+const FILING_METHOD_ID = process.env.FILING_METHOD_ID
+const WEBSITE_ID = process.env.WEBSITE_ID
+
+params = {
+	filing_method_id: FILING_METHOD_ID,
+	website_id: WEBSITE_ID
+}
+
+token = baseRequest.request.token({ path: '/signed-forms' });
+
+baseRequest.request.get({ path: '/signed-forms', token: token, queryParams: params });
