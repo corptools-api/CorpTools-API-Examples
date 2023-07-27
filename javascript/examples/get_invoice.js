d6507d7e3e93adfb0fc7c762c98314ff29d43556
Corptools API <63736943+corporatetools@users.noreply.github.com>
2023-07-27 13:37:51 -0700

    [FILE-2185] built out get-document by id examples for all projects (#46)
    Co-authored-by: Madison de Groot <madisondegroot@corporatetools.com>

--- javascript/examples/get_invoice.js
+++ javascript/examples/get_invoice.js
@@ -3,6 +3,8 @@ const path = require('path');
 process.chdir(path.resolve(__dirname, '../../'));
 require('dotenv').config();
 
+// Example of GET /invoices/:invoice_id
+
 const INVOICE_ID = process.env.INVOICE_ID;
 
 token = baseRequest.request.token({ path: `/invoices/${INVOICE_ID}` });
