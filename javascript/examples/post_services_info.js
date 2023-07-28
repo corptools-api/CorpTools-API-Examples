5aafff3fa7320e678c8e57d8cc4e99714b656adb
Matt Perry <mattperry@corporatetools.com>
2023-07-28 14:24:39 -0700

    FILE-2210: POST /services/:id/info in JavaScript

---
+++ javascript/examples/post_services_info.js
@@ -0,0 +1,28 @@
+const baseRequest = require('../base_request.js').baseRequest
+const path = require('path');
+process.chdir(path.resolve(__dirname, '../../'));
+require('dotenv').config();
+
+const fs = require('fs');
+const currentWorkingDirectory = process.cwd();
+const filePath = `${currentWorkingDirectory}/data/services/add_info_corp.json`;
+
+// Example POST /services/:id/info
+
+const SERVICE_ID = process.env.SERVICE_ID
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the JSON file:', err);
+    return;
+  }
+
+  try {
+	body = JSON.parse(data);
+	url_path = `/services/${SERVICE_ID}/info`;
+	token = baseRequest.request.token({ path: url_path, body: body });
+	baseRequest.request.post({ path: url_path, token: token, body: body }); 
+  } catch (parseError) {
+    console.error('Error parsing form_data:', parseError);
+  }
+});
