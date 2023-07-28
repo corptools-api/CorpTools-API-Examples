0de21668cc310dfc3a8a9b299be42b209b3a8106
Matt Perry <mattperry@corporatetools.com>
2023-07-28 14:17:01 -0700

    FILE-2210: POST /services/:id/info in Ruby

--- ruby/examples/post_services_info.rb
+++ ruby/examples/post_services_info.rb
@@ -7,4 +7,4 @@ SERVICE_ID = ENV['SERVICE_ID']
 current_directory="#{File.dirname(File.expand_path(__FILE__))}"
 body = JSON.parse(File.read("#{current_directory}/../../data/services/add_info_corp.json"))
 
-BaseRequestRoute.request(:post, "/services/#{SERVICE_ID}/info", body: body)
+BaseRequestRoute.request(:post, "/services/#{SERVICE_ID}/info", body: body.to_json)
