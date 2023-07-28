20ec05416153638401d3ad2b5e8873d756098741
Madison de Groot <madisondegroot@corporatetools.com>
2023-07-28 15:32:42 -0700

    [FILE-2186] modified js axios request for downloads

--- javascript/base_request_axios.js
+++ javascript/base_request_axios.js
@@ -2,20 +2,22 @@ require('dotenv').config({ path: '../.env' });
 const axios = require('axios');
 const CryptoJS = require("crypto-js");
 const sign = require('jwt-encode');
+const fs = require("fs").promises;
+const pathModule = require("path");
 
-const DEBUG			= process.env.DEBUG;
-const API_URL		= process.env.API_URL;
-const ACCESS_KEY 	= process.env.ACCESS_KEY;
-const SECRET_KEY 	= process.env.SECRET_KEY;
+const DEBUG = process.env.DEBUG;
+const API_URL = process.env.API_URL;
+const ACCESS_KEY = process.env.ACCESS_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
 
 exports.request = {
-	token: function({ path, body = null }) {
+	token: function ({ path, body = null }) {
 		let token = "";
-		let header = { access_key: ACCESS_KEY};
+		let header = { access_key: ACCESS_KEY };
 		let payload = {
 			path: path
 		};
-		
+
 		if (body) {
 			payload.content = CryptoJS.SHA256(JSON.stringify(body)).toString(CryptoJS.enc.Hex);
 		} else {
@@ -25,23 +27,41 @@ exports.request = {
 		if (DEBUG) console.log(`token=${token}`);
 		return token;
 	},
-	base_request: function({ method, path, token, body = {}}) {
+	base_request: async function ({ method, path, token, body = {} }) {
 		let url = API_URL + path;
 		if (DEBUG) console.log(`Axios: ${method} request to url=${url} body=${body}`);
-		axios({
-		  method: method,
-		  url: url,
-		  data: body,
-		  headers: {
-		    'Authorization': 'Bearer ' + token,
-		  },
-		}).then(response => {
-		  console.log('response:', response.data)
-		}).catch(error => {
-		  console.log('error:', error.message, error.config.data)
-		})
+
+		try {
+			const response = await axios({
+				method: method,
+				url: url,
+				data: body,
+				responseType: 'arraybuffer',
+				headers: {
+					'Authorization': 'Bearer ' + token,
+				},
+			});
+
+			// Check content type and save files accordingly (if needed)
+			const contentType = response.headers['content-type'];
+			if (contentType.includes('image/png')) {
+				const buffer = Buffer.from(response.data);
+				const filePath = pathModule.join(__dirname, 'documents', 'get_document_page_response.png');
+				await fs.writeFile(filePath, buffer);
+				console.log('PNG file saved as get_document_page_response.png');
+			} else if (contentType.includes('application/pdf')) {
+				const buffer = Buffer.from(response.data);
+				const filePath = pathModule.join(__dirname, 'documents', 'get_document_download_response.pdf');
+				await fs.writeFile(filePath, buffer);
+				console.log('PDF file saved as get_document_download_response.pdf');
+			} else {
+				console.log('response:', response.data);
+			}
+		} catch (error) {
+			console.log('error:', error.message, error.config.data);
+		}
 	},
-	delete: function({ path, token, body = {} }) {
+	delete: function ({ path, token, body = {} }) {
 		this.base_request({ method: 'DELETE', path: path, token: token, body: body });
 	},
 	get: function ({ path, token, queryParams = '' }) {
@@ -50,10 +70,10 @@ exports.request = {
 			// read in query parameters to set on the URL
 			const params = new URLSearchParams();
 			for (let key in queryParams) {
-			  if (queryParams.hasOwnProperty(key)) {
-			    const value = queryParams[key];
-			    params.append(key, value);
-			  }
+				if (queryParams.hasOwnProperty(key)) {
+					const value = queryParams[key];
+					params.append(key, value);
+				}
 			}
 			path += '?' + params.toString();
 		}
