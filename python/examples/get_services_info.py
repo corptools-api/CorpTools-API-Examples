7ec3c3858a61ce08724e6342bf76611e2bfc6eae
Matt Perry <mattperry@corporatetools.com>
2023-07-28 13:41:07 -0700

    FILE-2211: GET /services/:id/info in Python

---
+++ python/examples/get_services_info.py
@@ -0,0 +1,26 @@
+import pprint
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from request import BaseRequest
+from dotenv import dotenv_values
+
+config = dotenv_values()
+
+# Example of GET /services/:id/info
+
+class GetServicesInfoRequest(BaseRequest):
+    def __init__(self):
+        super().__init__()
+    
+    def get_info(self, service_id):
+        return self.make_request('GET', f'/services/{service_id}/info')
+
+service_id = config['SERVICE_ID']
+
+request = GetServicesInfoRequest()
+response = request.get_info(service_id)
+
+pprint.pprint(response)
