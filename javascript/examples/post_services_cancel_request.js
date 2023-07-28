488e3cbc895f267b066d75eaedce842e83473050
Matt Perry <mattperry@corporatetools.com>
2023-07-28 15:32:17 -0700

    FILE-2209: POST /services/:id/cancel-request in JavaScript

---
+++ javascript/examples/post_services_cancel_request.js
@@ -0,0 +1,12 @@
+const baseRequest = require('../base_request.js').baseRequest
+const path = require('path');
+process.chdir(path.resolve(__dirname, '../../'));
+require('dotenv').config();
+
+// Example POST /services/:id/cancel-request
+
+const SERVICE_ID = process.env.SERVICE_ID
+
+const url_path = `/services/${SERVICE_ID}/cancel-request`;
+const token = baseRequest.request.token({ path: url_path });
+baseRequest.request.post({ path: url_path, token: token }); 
