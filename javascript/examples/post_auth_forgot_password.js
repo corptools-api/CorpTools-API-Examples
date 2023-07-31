25ecc9b1b0666a76f5225a69ed353e1801dfd9b6
Matt Perry <mattperry@corporatetools.com>
2023-07-31 15:38:57 -0700

    POST /auth/forgot-password in JavaScript

---
+++ javascript/examples/post_auth_forgot_password.js
@@ -0,0 +1,24 @@
+const baseRequest = require('../base_request.js').baseRequest
+
+const path = require('path');
+process.chdir(path.resolve(__dirname, '../../'));
+require('dotenv').config();
+
+// Example of POST /auth/forgot-password
+
+const URL = process.env.CALLBACK_URL;
+
+const EMAIL = process.env.EMAIL;
+const SUBDOMAIN = process.env.SUBDOMAIN;    // optional
+const WEBSITE_ID = process.env.WEBSITE_ID;  // optional
+
+let body =
+{
+  email_address: EMAIL,
+  // subdomain: SUBDOMAIN,
+  website_id: WEBSITE_ID
+};
+
+token = baseRequest.request.token({ path: '/auth/forgot-password', body: body });
+
+baseRequest.request.post({ path: '/auth/forgot-password', token: token, body: body });
