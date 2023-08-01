c9a1cbc94479fb70b5e91f5651d899fa4583ac67
Matt Perry <mattperry@corporatetools.com>
2023-08-01 15:02:50 -0700

    FILE-2204: GET /resources/:id in C#

---
+++ csharp/examples/GetResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Examples.examples
+{
+    // Example of GET /resources/:id
+    public class GetResource : BaseRequest
+    {
+        private string _resourceId;
+
+        public GetResource()
+        {
+            _resourceId = Environment.GetEnvironmentVariable("AGENCY_RESOURCE_ID");
+            Console.WriteLine($"GetResource: _resourceId={_resourceId}");
+        }
+
+        public override void SendRequest()
+        {
+            GetRequest($"resources/{_resourceId}");
+        }
+    }
+}
+
