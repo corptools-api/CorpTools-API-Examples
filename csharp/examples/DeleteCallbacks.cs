4b7d86248aa0db8d78fb5d21071b199febf3706d
Matt Perry <mattperry@corporatetools.com>
2023-08-02 11:59:35 -0700

    indentation fix

--- csharp/examples/DeleteCallbacks.cs
+++ csharp/examples/DeleteCallbacks.cs
@@ -4,7 +4,7 @@ namespace Examples.examples
     // Example of DELETE /callbacks/:callback_id
 
     public class DeleteCallbacks : BaseRequest
-	{
+    {
         private string _callbackId;
 
         public DeleteCallbacks()
