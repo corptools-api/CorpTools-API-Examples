733707873f822e747875e464087e0f23ea869d39
Madison de Groot <madisondegroot@corporatetools.com>
2023-08-03 13:50:43 -0700

    [FILE-2259] updated base ruby request to handle query_params differently from body and corrected one example

--- ruby/examples/delete_shopping_cart.rb
+++ ruby/examples/delete_shopping_cart.rb
@@ -6,8 +6,8 @@ COMPANY_ID = ENV['COMPANY_ID']
 ITEM_ID    = ENV['SHOPPING_CART_ITEM_ID']
 
 parameters = {
-	company_ids: [COMPANY_ID],
-	item_ids: [ITEM_ID]
+   company_ids: [COMPANY_ID],
+   item_ids: [ITEM_ID]
 }.to_json
 
-BaseRequestRoute.request(:delete, '/shopping-cart', query_params: parameters)
+BaseRequestRoute.request(:delete, '/shopping-cart', body: parameters)
