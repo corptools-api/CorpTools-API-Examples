47b3b5cfee6e2d292e3f559e55ce2bc01f451b8d
Matt Perry <mattperry@corporatetools.com>
2023-08-07 13:33:56 -0700

    FILE-2265: comment out end to end EIN tax id checkout in C#

--- csharp/Examples.cs
+++ csharp/Examples.cs
@@ -64,7 +64,7 @@ namespace Examples
             // requestPostShoppingCart();
             //requestPostShoppingCartCheckout();
 
-             einTaxIdCheckout();
+            // einTaxIdCheckout();
 
             // ListenForCallback();
         }
